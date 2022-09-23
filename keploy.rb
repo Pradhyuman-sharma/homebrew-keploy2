73975289d022312409c31e61c345d77e2676c740
Pradhyuman-sharma <sharmapuru0642@gmail.com>
2022-09-23 17:28:19 +0530

    feat: adding brew formula

--- keploy.rb
+++ keploy.rb
@@ -1,31 +1,30 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-require "language/go"
 require "language/node"
+require "fileutils"
 class Keploy < Formula
-  desc "Unit Testing Toolkit that creates test-cases and data mocks from API calls."
+  desc "Unit Testing Toolkit that creates test-cases and data mocks from API calls"
   homepage "https://keploy.io"
   url "https://github.com/keploy/keploy/archive/refs/tags/v0.5.5.tar.gz"
   sha256 "37a4a27d30ed831c8fd4f60609df4c6b3ce1633fd468696e8098a35a16140377"
   license "Apache-2.0"
 
-  depends_on "go" => :build
   depends_on "gatsby-cli" => :build
   depends_on "node@14" => :build
- 
+  depends_on "go" => :build
 
   def install
-    system "git","clone","https://github.com/keploy/ui.git"
+    system "git", "clone", "https://github.com/keploy/ui.git"
     Dir.chdir("ui") do
-       system "npm", "install"
-       system "gatsby", "build"
+      system "npm", "install", *Language::Node.local_npm_install_args
+      system "gatsby", "build"
     end  
-    system "cp", "-R" , "ui/public", "web"
-    system "rm", "-rf", "ui"
+    FileUtils.cp_r "ui/public", "web"
+    FileUtils.rm_rf "ui"
     Dir.chdir("cmd/server") do
-       system "go", "build", "-o" , "keploy"   
-       bin.install "keploy"
+      system "go", "build", "-o", "keploy"   
+      bin.install "keploy"
     end      
+  end
+  test do 
+    assert_match "👍 connect to http://localhost:8081/ for GraphQL playground", shell_output("#{bin}/keploy")
   end
 end
